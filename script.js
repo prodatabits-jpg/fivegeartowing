b535bbf484e888e9428cc4096df750304d9c650c
prodatabits-jpg <prodatabits@gmail.com>
2026-07-11 21:37:25 +0530

    Delete script.js

--- script.js
+++
@@ -1,9 +0,0 @@
-const hero=document.querySelector('.hero');
-const heroImages=['truck1.jpeg','truck2.jpeg','truck3.jpeg','truck4.jpeg','truck5.jpeg'];
-let i=0;
-function rotateHero(){
- hero.style.backgroundImage=`linear-gradient(rgba(0,0,0,.45),rgba(0,0,0,.45)),url('${heroImages[i]}')`;
- i=(i+1)%heroImages.length;
-}
-rotateHero();
-setInterval(rotateHero,4000);
